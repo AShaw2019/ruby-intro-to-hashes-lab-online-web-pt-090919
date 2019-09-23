cb4b6a335694e3534bf1eb5f1f3a58bc0b51e249
Ashley Twomey <aprier091012@gmail.com>
2019-09-23 01:41:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,8 +11,17 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-	monopoly =  {:railroads => {:pieces => 4}}
- monopoly_with_second_tier [:railroads] = [:names] = {}
+monopoly = {
+
+		:railroads=>
+		{
+		:pieces=>4,
+		:names=>{},
+		:rent_in_dollars=>{}
+		}
+	}
+
+end	
 end
 
 def monopoly_with_third_tier
